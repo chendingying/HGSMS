fe6206f51db25255571a6c5879bcc82346627885
chendy <chen@dingying.com>
2018-12-17 17:27:42 +0800

    init

--- src/main/java/com/hgsms/mapper/admin/GateLogMapper.java
+++
@@ -1,9 +0,0 @@
-package com.hgsms.mapper.admin;
-
-
-
-import com.hgsms.model.admin.GateLog;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface GateLogMapper extends Mapper<GateLog> {
-}
--- src/main/java/com/hgsms/mapper/admin/GroupTypeMapper.java
+++
@@ -1,8 +0,0 @@
-package com.hgsms.mapper.admin;
-
-
-import com.hgsms.model.admin.GroupType;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface GroupTypeMapper extends Mapper<GroupType> {
-}
